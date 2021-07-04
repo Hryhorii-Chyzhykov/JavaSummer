fcf60ce262cc9b21befe54bf8db97ff144989418
Hryhorii-Chyzhykov <86090632+Hryhorii-Chyzhykov@users.noreply.github.com>
2021-07-04 20:59:32 +0300

    Delete Lec3 directory

--- Lec3/lec3/GlobalConstsnts.java
+++
@@ -1,6 +0,0 @@
-package lec3;
-
-public interface GlobalConstsnts {
-    int MIN_GLOBAL_VALUE = 0;
-    int MAX_GLOBAL_VALUE = 100;
-}
--- Lec3/lec3/Main.java
+++
@@ -1,9 +0,0 @@
-package lec3;
-
-public class Main {
-    public static void main(String[] args) {
-
-        Controller controller = new Controller(new Model(), new View());
-        controller.userProcess();
-    }
-}
