8478296150bac6524c312a04092be384c230a678
Hryhorii-Chyzhykov <86090632+Hryhorii-Chyzhykov@users.noreply.github.com>
2021-07-04 21:00:02 +0300

    Add files via upload

---
+++ Lec3/lec3/Model.java
@@ -0,0 +1,35 @@
+package lec3;
+
+import java.util.ArrayList;
+
+public class Model {
+    private int value;
+    private int minValue;
+    private int maxValue;
+    private ArrayList<Integer> attempts = new ArrayList<>();
+
+
+    public void setValue(int value) {this.value = value;}
+    public int getValue() {return value;}
+    public void setMaxValue(int maxValue) {this.maxValue = maxValue;}
+    public int getMaxValue() {return maxValue;}
+    public void setMinValue(int minValue) {this.minValue = minValue;}
+    public int getMinValue() {return minValue;}
+    public ArrayList getAttempts(){return attempts;}
+
+    public boolean compareValue (int userValue){
+        attempts.add(userValue);
+        if (value == userValue){
+            return false;
+        } else if (userValue > value){
+            maxValue = userValue;
+        } else {
+            minValue = userValue;
+        }
+        return true;
+    }
+
+    public int randomValue(){
+        return (int)Math.ceil(Math.random()* (maxValue - minValue - 1) + minValue);}
+
+}
---
+++ Lec3/lec3/View.java
@@ -0,0 +1,12 @@
+package lec3;
+
+public class View {
+    public static final String ENTER_VALUE = "Enter a random number between ";
+    public static final String ENTER_AN_INTEGER = "Enter an integer number";
+    public static final String OUT_OF_RANGE = "Number is out of range";
+    public static final String WIN = "You win";
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+}
